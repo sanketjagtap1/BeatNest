795a91c9b52e648e4f4d6641b2a0f69d76430dcc
“Sanket <Sanket.jagtap@bajajfinserv.in>
2024-11-23 10:54:21 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BeatNest',
+  webDir: 'www'
+};
+
+export default config;
